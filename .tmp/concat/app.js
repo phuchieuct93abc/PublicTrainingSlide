c654a0166836026eca901e2b480ea2910bfd532a
phuchieuct <phuchieuct93@gmail.com>
2017-05-16 07:57:55 +0700

    Update target

--- .tmp/concat/app.js
+++ .tmp/concat/app.js
@@ -237,6 +237,34 @@ app.controller("hackathonDemo", function ($scope) {
 
 
 })
+app.controller("letStart", function ($timeout) {
+    Reveal.addEventListener('letStart', function () {
+        $timeout(function () {
+
+            // Grab the current date
+            // Set some date in the past. In this case, it's always been since Jan 1
+            // Calculate the difference in seconds between the future and current date
+            var diff = 60 * 80;
+            // Instantiate a coutdown FlipClock
+            $('.my-clock').FlipClock(diff, {
+                clockFace: 'SecondCounter',
+                showSeconds: false,
+                countdown: true,
+                callbacks: {
+                    stop: function () {
+                        $('#myModal').modal({backdrop: true})
+                    }
+                }
+            });
+
+        }, 1000)
+    }, false);
+
+
+
+
+
+})
 app.controller("liveCodeDirective", function ($timeout) {
     var mySwiper;
     Reveal.addEventListener('liveCodeDirective', function () {
