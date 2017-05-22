34ac23692368bf8c5eed496ee2044df6cfb7433d
phuchieuct <phuchieuct93@gmail.com>
2017-05-22 21:01:49 +0700

    update

---
+++ controllers/axon-directive.js
@@ -0,0 +1,12 @@
+app.directive('axonLogo', function () {
+
+    return {
+        replace: true,
+        templateUrl: "slides/axon-directive.html",
+        controller: function ($scope) {
+
+
+        }
+    }
+
+})
--- controllers/controller.js
+++ controllers/controller.js
@@ -28,24 +28,6 @@ app.directive("axon", function () {
         controller: function ($element) {
 
 
-            var gradient_percent = 0,
-                    gradient_offset = {
-                        min: -40,
-                        max: 140
-                    },
-                    speed = 2,
-                    gradient_color = 'rgba(6, 155, 251, 1)',
-                    empty_color = 'rgba(0, 0, 0 , 0)';
-
-            setInterval(function () {
-                gradient_percent += speed;
-
-                if (gradient_percent > gradient_offset.max) {
-                    gradient_percent = gradient_offset.min;
-                }
-
-              //   $($element).find('.slide_effect').css('background-image', '-webkit-radial-gradient(' + gradient_percent + '% 50%, 3em 2em, ' + gradient_color + ' 0%, ' + empty_color + ' 100%)')
-            }, 2000);
 
         }
     }
